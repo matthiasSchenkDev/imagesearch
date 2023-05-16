1641a0c08bb9a06dcfdffb4ab0d289cd0b02e4c8
Matthias Schenk <>
2023-05-17 00:04:03 +0200

    add results fragment toolbar

--- app/src/main/java/com/example/imagesearch/ResultsFragment.kt
+++ app/src/main/java/com/example/imagesearch/ResultsFragment.kt
@@ -1,21 +1,34 @@
 package com.example.imagesearch
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 
-class ResultsFragment : Fragment() {
+class ResultsFragment : Fragment(R.layout.fragment_results) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(view) {
+            val toolbar = findViewById<Toolbar>(R.id.resultsFragmentToolbar)
+            val searchViewMenuItem = toolbar.menu.findItem(R.id.action_results_fragment_search)
+            val searchView = searchViewMenuItem.actionView as SearchView
+            searchView.setOnQueryTextListener(object : OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    query?.let { onNewQuery(it) }
+                    return true
+                }
 
+                override fun onQueryTextChange(newText: String?) = true
+
+            })
+        }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_results, container, false)
+    private fun onNewQuery(query: String) {
+        TODO("Not yet implemented")
+    }
 
 }
