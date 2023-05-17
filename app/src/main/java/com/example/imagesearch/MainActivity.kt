bd6f2ecde96b95c1a2be3ec56779ca4cee6333d6
Matthias Schenk <>
2023-05-17 12:11:09 +0200

    fix hilt crash on appstart

--- app/src/main/java/com/example/imagesearch/MainActivity.kt
+++ app/src/main/java/com/example/imagesearch/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.imagesearch
 
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.activity_main)
